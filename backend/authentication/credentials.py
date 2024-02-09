613ac9e0426a36286ed9c18222e010bfe3296083
Höck Alexander <alexander.hoeck@edu.fh-joanneum.at>
2024-02-09 23:26:40 +0100

    Added class and enum for authentication header

---
+++ backend/authentication/credentials.py
@@ -0,0 +1,30 @@
+
+
+class Credentials:
+    def __init__(self, set_id: str, name: str, secret: str, json: dict):
+        self._id = set_id
+        self._name = name
+        self._secret = secret
+        self._json = json
+
+    @property
+    def json(self) -> dict:
+        return self._json
+
+    @json.setter
+    def json(self, value):
+        set_id, name, secret = value
+        self._json = {
+            "auth": {
+                "identity": {
+                    "methods": [
+                        "application_credential"
+                    ],
+                    "application_credential": {
+                        "id": set_id,
+                        "name": name,
+                        "secret": secret
+                    }
+                }
+            }
+        }
---
+++ backend/authentication/credentialsdata.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+# Add your own credentials for connecting to openstack api
+class CredentialData(Enum):
+    ID = ""
+    NAME = ""
+    SECRET = ""
