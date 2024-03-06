a86dc991bb79998bd53caee8a700886477793fc2
Alexander Höck <alexander.hoeck@edu.fh-joanneum.at>
2024-03-06 18:42:55 +0100

    Deleted urls

--- backend/requestclasses/endpoints.py
+++ backend/requestclasses/endpoints.py
@@ -2,16 +2,16 @@ from enum import Enum
 
 
 class EndPoints(Enum):
-    IDENTITY = 'https://os2020.fh-joanneum.at:5000/v3'
-    TOKEN_URL = '/auth/tokens'
+    IDENTITY = ""
+    TOKEN_URL = "/auth/tokens"
     HEADERS = {"Content-Type": "application/json"}
 
-    COMPUTE = "https://os2020.fh-joanneum.at:8774/v2.1"
+    COMPUTE = ""
     C_SERVERS = "/servers"
     C_SERVERS_DETAIL = "/detail"
     C_FLAVORS = "/flavors"
 
-    NETWORK = " https://os2020.fh-joanneum.at:9696/v2.0"
+    NETWORK = ""
     N_PORTS = "/ports"
     N_FLOATINGIPS = "/floatingips"
     N_ROUTERS = "/routers"
